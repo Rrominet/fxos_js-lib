d8da598497e6ce508b757a77016ba08d8e069ec2
Romain <romain.gilliot@motion-live.com>
2026-01-16 16:42:16 +0100

    finished advice form

--- js/bundle.js
+++ js/bundle.js
@@ -48,4 +48,31 @@ class bundle
                 return;
         }
     }
-}
+
+    static async write(filepath, _data, cb=null)
+    {
+        if (typeof(_data) == "object")
+            _data = JSON.stringify(_data);
+
+        const data = {"file" : filepath, "location" : location, content : _data};
+        try
+        {
+            const response = await fetch(bundle.url + "/set", {
+                method : "POST" ,
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify(data)
+            });
+            const result = await response.json();
+            if (cb)
+                cb(result);
+            return result;
+        }
+        catch(e)
+        {
+            console.error("bundle request error : " + e);
+            return;
+        }
+    }
+    }
